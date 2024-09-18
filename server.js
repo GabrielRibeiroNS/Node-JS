8a5c30823b48a406d322d7bfb93ae048703eb4f6
Gabriel Ribeiro Nicácio Silva <gabrielribeironi7@gmail.com>
2024-09-17 22:04:49 -0300

    Update server.js

--- server.js
+++ server.js
@@ -6,6 +6,16 @@
 
  const app = express()
 
+ const users = []
+
+ app.post('/usuarios', (req , res) => {
+
+    console.log(req)
+
+    res.send('ok. aqui deu certo')
+
+ })
+
 //req = requisição / res = resposta
  app.get('/usuarios', (req, res) => {
     res.send('Legal, deu certo a resposta!')
@@ -15,3 +25,14 @@
  //Definir a porta onde vai estar o acesso do localhost para buscar no navegador por exemplo:
  //http://localhost:3000/usuarios
  app.listen(3000)
+
+
+ /*
+    Criar nossa API de Usuários
+
+    -Criar um usuário
+    -Listar todos os usuários
+    -Editar um usuário ou varios
+    -Deletar um usuário
+
+ */
